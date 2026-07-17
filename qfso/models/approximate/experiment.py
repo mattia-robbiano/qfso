c5fdbf52e9062befcc54852d710f895bf36243d4
mattia-robbiano <mattiarobbiano@gmail.com>
2026-07-17 15:09:25 +0300

    CLUSTER general cleanup

--- qfso/models/approximate/experiment.py
+++ qfso/models/approximate/experiment.py
@@ -18,26 +18,19 @@ def _mmd(model_coeffs, target_coeffs, n_ks):
     diff = model_coeffs - target_coeffs
     return jnp.sum(diff*diff)/n_ks
 
-# Definizione dell'ottimizzatore globale. Adam con un lr standard parte molto meglio di SGD.
 optimizer = optax.adam(learning_rate=0.5)
-
-# n_ks è all'indice 4, opt_state (dinamico) è all'indice 5
 @partial(jax.jit, static_argnums=4)
 def train_step(fourier_matrix, latent_probs, sparse_ks, target_coeffs, n_ks, opt_state):
-    # Calcolo congiunto di loss e gradienti
     batch_loss, grad = jax.value_and_grad(loss, argnums=0)(fourier_matrix, latent_probs, sparse_ks, target_coeffs, n_ks)
     
-    # Passaggio a optax: ignoriamo l'ultima riga della matrice che deve restare costante a 1
     trainable_grad = grad[:-1, :]
     trainable_fourier = fourier_matrix[:-1, :]
     
     updates, new_opt_state = optimizer.update(trainable_grad, opt_state, trainable_fourier)
     new_trainable_fourier = optax.apply_updates(trainable_fourier, updates)
     
-    # Riapplicazione del clip range [-1, 1]
     new_trainable_fourier = jnp.clip(new_trainable_fourier, min=-1.0, max=1.0)
     
-    # Ricostruzione della matrice completa
     new_fourier_matrix = fourier_matrix.at[:-1, :].set(new_trainable_fourier)
     
     return new_fourier_matrix, new_opt_state, batch_loss
@@ -50,7 +43,6 @@ if __name__ == "__main__":
     sigma = 7.1
     path = "../../../data/"
     
-    # Modifica per aumentare l'esposizione: più batch per epoca
     batch_size = 10_000
     num_batches = 200 
 
@@ -71,12 +63,10 @@ if __name__ == "__main__":
     model = LinCombModel(n_qubits, m)
     key = jax.random.PRNGKey(35)
     
-    # Manteniamo l'inizializzazione strettamente positiva
     model.fourier_matrix = model.fourier_matrix.at[:-1,:].set(
         jax.random.uniform(key, shape=(n_qubits,m), minval=0.5, maxval=1.0)
     )
 
-    # Inizializzazione dello stato interno di optax (momenti, varianze) solo sulla porzione addestrabile
     opt_state = optimizer.init(model.fourier_matrix[:-1, :])
 
     print("Done.\nStarting actual work")
